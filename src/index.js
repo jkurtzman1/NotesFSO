e27c20a4f52623ebeaeb3e8c48c701a6033c5033
Jason Kurtzman <jasonkurtzman@yahoo.com>
2023-01-25 19:46:29 -0600

    Messed around with inline and external styling. Also fixed warning regarding not setting exported default to object before exporting in services/notes.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 //const promise2 = axios.get("https://localhost:3001/foobar");
 //console.log(promise2);
 
